1043d39882be0b656d599b6f71ab9eacb3e6b1ca
Ayush Goel <ayushgoel111@gmail.com>
2012-09-18 16:45:43 +0530

    Fine tuned calculation of rowHeight for any tweet

--- TweetMini/tweet.m
+++ TweetMini/tweet.m
@@ -16,21 +16,32 @@
 #define CELL_CONTENT_WIDTH 320.0f
 #define CELL_CONTENT_MARGIN 10.0f
 #define CELL_TITLE_HEIGHT 30.0f
+#define CELL_IMAGE_WIDTH 60.0f
 
 -(CGFloat) rowHeight
 {    
-    CGSize constraint = CGSizeMake(CELL_CONTENT_WIDTH - (CELL_CONTENT_MARGIN * 2), 20000.0f);
+    CGSize constraint = CGSizeMake(CELL_CONTENT_WIDTH - (CELL_CONTENT_MARGIN * 2) - CELL_IMAGE_WIDTH, 20000.0f);
     CGSize size;
 
     size = [self.text sizeWithFont:[UIFont systemFontOfSize:FONT_SIZE] constrainedToSize:constraint lineBreakMode:UILineBreakModeWordWrap];
-    CGFloat height = MAX(size.height, 44.0f);
+//    CGFloat height = MAX(size.height, 34.0f);
+    CGFloat height = size.height;
     
-    return height + (CELL_CONTENT_MARGIN * 2) + CELL_TITLE_HEIGHT;
+    return height + (CELL_CONTENT_MARGIN) + CELL_TITLE_HEIGHT;
 }
 
 -(NSString *) description
 {
     return [NSString stringWithFormat:@"User:%@ Text:%@", user, text];
+}
+
+- (id)init {
+    self = [super init];
+    if (self) {
+            // Initialize self.
+        self.user = [[miniUser alloc] init];
+    }
+    return self;
 }
 
 @end
