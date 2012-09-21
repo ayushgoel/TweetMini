e784fdd63b2e4c8008b02757904696c34b202045
Ayush Goel <ayush.g@directi.com>
2012-09-21 17:15:31 +0530

    removed comments

--- TweetMini/tweetMiniViewController.m
+++ TweetMini/tweetMiniViewController.m
@@ -12,12 +12,10 @@
 #import "HomeViewController.h"
 
 @interface tweetMiniViewController ()
-//@property (atomic) ACAccount *account; 
 @end
 
 @implementation tweetMiniViewController
 @synthesize statusLabel;
-//@synthesize account;
 
 -(UIAlertView *) getAlertViewWithMessage: (NSString *) msg{
     return [[UIAlertView alloc] initWithTitle:@"Twitter Authorisation" message:msg delegate:self cancelButtonTitle:@"Exit" otherButtonTitles: nil];
@@ -32,32 +30,17 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-//    NSURL *url = [NSURL URLWithString:@"http://search.twitter.com/search.json?q=Twitter%20API&result_type=mixed"];
-//    
-//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-//    
-//    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
-//        NSLog(@"Name: %@ %@", [JSON valueForKeyPath:@"max_id"], [JSON valueForKeyPath:@"results"]);
-//    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON){
-//        NSLog(@"Error: %@", error);
-//    }];
-//    [operation start];
-    
     statusLabel.text = @"Trying to get your twitter credentials";
     
     ACAccountStore *accountStore = [[ACAccountStore alloc] init];
     ACAccountType *accountType = [accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
-        //NSLog(@"Can Send: %i", [TWTweetComposeViewController canSendTweet]);
     if([TWTweetComposeViewController canSendTweet]){
         [accountStore requestAccessToAccountsWithType:accountType withCompletionHandler: ^(BOOL granted, NSError *error){
             if(granted){
                 statusLabel.text = @"Twitter Account verified";
-//                self.account = [[accountStore accountsWithAccountType:accountType] lastObject];
-//                         NSLog(@"User: %@", account);
                 [self performSegueWithIdentifier:@"userLoggedIn" sender:self];
             }
             else {
-                    //                NSLog(@"Not granted %@", error);
                 UIAlertView *alert = [self getAlertViewWithMessage: @"Please give permission to access your twitter account in the Settings, then try again!"];
                 [alert show];
             }
@@ -71,7 +54,6 @@
 
 -(void) alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex
 {
-        //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"prefs:root=General&path=Network"]];
         //Need something better to exit application
     exit(1);
 }
