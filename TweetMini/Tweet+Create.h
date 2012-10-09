9b120ac7e9ef118e3d4e9e2e88e37500b7f75743
Ayush Goel <ayush.g@directi.com>
2012-10-09 15:13:51 +0530

    added category over Tweet

---
+++ TweetMini/Tweet+Create.h
@@ -0,0 +1,13 @@
+//
+//  Tweet+Create.h
+//  TweetMini
+//
+//  Created by Ayush on 09/10/12.
+//  Copyright (c) 2012 Ayush. All rights reserved.
+//
+
+#import "Tweet.h"
+
+@interface Tweet (Create)
+
+@end
---
+++ TweetMini/Tweet+Create.m
@@ -0,0 +1,13 @@
+//
+//  Tweet+Create.m
+//  TweetMini
+//
+//  Created by Ayush on 09/10/12.
+//  Copyright (c) 2012 Ayush. All rights reserved.
+//
+
+#import "Tweet+Create.h"
+
+@implementation Tweet (Create)
+
+@end
