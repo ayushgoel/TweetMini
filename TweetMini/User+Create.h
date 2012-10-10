57481351a93bc4861003e0599fbef659cc93756c
Ayush Goel <ayush.g@directi.com>
2012-10-10 16:49:08 +0530

    Added category files on User

---
+++ TweetMini/User+Create.h
@@ -0,0 +1,13 @@
+//
+//  User+Create.h
+//  TweetMini
+//
+//  Created by Ayush on 10/10/12.
+//  Copyright (c) 2012 Ayush. All rights reserved.
+//
+
+#import "User.h"
+
+@interface User (Create)
+
+@end
---
+++ TweetMini/User+Create.m
@@ -0,0 +1,13 @@
+//
+//  User+Create.m
+//  TweetMini
+//
+//  Created by Ayush on 10/10/12.
+//  Copyright (c) 2012 Ayush. All rights reserved.
+//
+
+#import "User+Create.h"
+
+@implementation User (Create)
+
+@end
