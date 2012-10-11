7c7c62bc6891de4f8e7d374c26dc1d1bdf710896
Ayush Goel <ayush.g@directi.com>
2012-10-11 13:04:00 +0530

    Completed wiring of SVC

--- TweetMini/SearchTwitterViewControllerViewController.h
+++ TweetMini/SearchTwitterViewControllerViewController.h
@@ -12,6 +12,16 @@
 @interface SearchTwitterViewControllerViewController : TParentViewController<UITableViewDataSource, UISearchBarDelegate>
 
 @property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (strong, nonatomic) NSMutableArray *searchResults;
 @property (strong, nonatomic) IBOutlet UITableView *tweetTable;
+@end
+
+@interface tweet : NSObject
+
+@property (nonatomic, strong) NSString *tweetID;
+@property (nonatomic, strong) NSString *text;
+@property (nonatomic, strong) NSString *userName;
+@property (nonatomic, strong) NSURL *userProfileImageURL;
+@property (nonatomic) CGFloat rowHeight;
 
 @end
--- TweetMini/SearchTwitterViewControllerViewController.m
+++ TweetMini/SearchTwitterViewControllerViewController.m
@@ -7,24 +7,70 @@
 //
 
 #import "SearchTwitterViewControllerViewController.h"
-#import "tweet.h"
 #import "Twitter/Twitter.h"
+#import "AFNetworking.h"
 
-@interface SearchTwitterViewControllerViewController ()
+@implementation tweet
+@synthesize tweetID, text, rowHeight, userName, userProfileImageURL;
+
+#define FONT_SIZE 14.0f
+#define CELL_CONTENT_WIDTH 320.0f
+#define CELL_CONTENT_MARGIN 10.0f
+#define CELL_TITLE_HEIGHT 30.0f
+#define CELL_IMAGE_WIDTH 60.0f
+
+- (CGFloat)rowHeight
+{
+    CGSize constraint = CGSizeMake(CELL_CONTENT_WIDTH - (CELL_CONTENT_MARGIN * 2) - CELL_IMAGE_WIDTH, 20000.0f);
+    CGSize size;
+    
+    size = [self.text sizeWithFont:[UIFont systemFontOfSize:FONT_SIZE] constrainedToSize:constraint lineBreakMode:NSLineBreakByWordWrapping];
+    CGFloat height = size.height;
+    
+    return height + (CELL_CONTENT_MARGIN) + CELL_TITLE_HEIGHT;
+}
+
+-(NSString *) description
+{
+    return [NSString stringWithFormat:@"Text:%@", text];
+}
 @end
 
 @implementation SearchTwitterViewControllerViewController
-@synthesize searchBar;
-@synthesize tweetTable;
+@synthesize searchBar = _searchBar;
+@synthesize searchResults = _searchResults;
+@synthesize tweetTable = _tweetTable;
 
--(UITableView *) getTableViewObject
-{
-    return self.tweetTable;
+-(UIAlertView *) getAlertViewWithMessage: (NSString *) msg{
+    return [[UIAlertView alloc] initWithTitle:@"Twitter Authorisation" message:msg delegate:self cancelButtonTitle:@"Exit" otherButtonTitles: nil];
 }
 
--(NSString *) getCellIdentifier
+-(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return @"tweetCell";
+    return [self.searchResults count];
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [[self.searchResults objectAtIndex:[indexPath row]] rowHeight];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString * cellIdentifier = @"tweetCell";
+    UITableViewCell * cell = nil;
+    
+    tweet *resTweet = [self.searchResults objectAtIndex:[indexPath row]];
+    cell = [self.tweetTable dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    if(cell == nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text = resTweet.userName;
+    cell.detailTextLabel.text = resTweet.text;
+    [cell.imageView setImageWithURL: resTweet.userProfileImageURL placeholderImage:[UIImage imageNamed:@"profile.gif"]];
+    return cell;
 }
 
 -(void) populateTweetWithSearch: (NSString *) searchTerm
@@ -40,14 +86,12 @@
                 id results = [JSON valueForKey:@"results"];
                 [results enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
                     tweet *tempTweet = [[tweet alloc] init];
-                    tempTweet.text = [obj valueForKey:@"text"];
-                    tempTweet.tweetId = [obj valueForKey:@"id"];
-
-                    tempTweet.user.name = tempTweet.user.screenName = [obj valueForKey:@"from_user"];
-                    tempTweet.user.userId = [[obj valueForKey:@"from_user_id"] intValue]; 
-                    tempTweet.user.profileImageURL = [NSURL URLWithString:[obj valueForKey:@"profile_image_url"]];
+                    tempTweet.text = [obj objectForKey:@"text"];
+                    tempTweet.tweetID = [obj objectForKey:@"id"];
+                    tempTweet.userName = [obj objectForKey:@"from_user"];
+                    tempTweet.userProfileImageURL = [obj objectForKey:@"profile_image_url"];
                     
-                    [self.TTimeline addObject:tempTweet];
+                    [self.searchResults addObject:tempTweet];
                 }];
                 
                 [self.tweetTable reloadData];
@@ -65,7 +109,7 @@
             [alert show];
         }
     }];
-    [searchBar resignFirstResponder];
+    [self.searchBar resignFirstResponder];
 }
 
 -(void) searchBarSearchButtonClicked:(UISearchBar *)searchBari
@@ -80,10 +124,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [self.tweetTable setDataSource:self];
     self.searchBar.delegate = self;
+    self.tweetTable.dataSource = self;
     self.tweetTable.delegate = self;
-    self.TTimeline = [[NSMutableArray alloc] init];
+    self.searchResults = [[NSMutableArray alloc] init];
 }
 
 - (void)viewDidUnload
