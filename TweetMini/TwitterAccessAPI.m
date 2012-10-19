25bbd03f5c96cfc5743ba666380c27425f233c76
Ayush Goel <ayush.g@directi.com>
2012-10-19 15:08:31 +0530

    moved authorization code in loggingInViewController

--- TweetMini/TwitterAccessAPI.m
+++ TweetMini/TwitterAccessAPI.m
@@ -12,16 +12,14 @@
 @synthesize accountStore = _accountStore;
 @synthesize accountType = _accountType;
 
-- (ACAccountStore *)accountStore
-{
+- (ACAccountStore *)accountStore {
     if (!_accountStore) {
         _accountStore = [[ACAccountStore alloc] init];
     }
     return _accountStore;
 }
 
-- (ACAccountType *)accountType
-{
+- (ACAccountType *)accountType {
     if (!_accountType) {
         _accountType = [self.accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
     }
@@ -29,13 +27,19 @@
 }
 
 -(UIAlertView *) getAlertViewWithMessage: (NSString *) msg{
-    return [[UIAlertView alloc] initWithTitle:@"Twitter Authorisation" message:msg delegate:self cancelButtonTitle:@"Exit" otherButtonTitles: nil];
+    return [[UIAlertView alloc] initWithTitle:@"Twitter Authorisation"
+                                      message:msg
+                                     delegate:self
+                            cancelButtonTitle:@"Exit"
+                            otherButtonTitles: nil];
 }
 
 - (void)withTwitterCallSelector:(SEL)willCallSelector withObject:(id)obj
 {    
     if([TWTweetComposeViewController canSendTweet]){
-        [self.accountStore requestAccessToAccountsWithType:self.accountType options:nil completion:^(BOOL granted, NSError *error) {
+        [self.accountStore requestAccessToAccountsWithType:self.accountType
+                                                   options:nil
+                                                completion:^(BOOL granted, NSError *error) {
             if(granted){
                 
                 [obj performSelector:willCallSelector];
@@ -43,13 +47,16 @@
             }
             else {
                 
-                UIAlertView *alert = [self getAlertViewWithMessage: @"Please give permission to access your twitter account in the Settings, then try again!"];
+                UIAlertView *alert = [self getAlertViewWithMessage: @"Please \
+                                      give permission to access your twitter \
+                                      account in the Settings, then try again!"];
                 [alert show];
             }
         }];
     }
     else {
-        UIAlertView *alert = [self getAlertViewWithMessage:@"Please log into Twitter in the Settings, then try again!"];
+        UIAlertView *alert = [self getAlertViewWithMessage:@"Please log into \
+                              Twitter in the Settings, then try again!"];
         [alert show];
     }
             
--- TweetMini/loggingInViewController.h
+++ TweetMini/loggingInViewController.h
@@ -7,8 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Accounts/Accounts.h"
+#import "Twitter/Twitter.h"
+#import "TwitterAccessAPI.h"
 
 @interface loggingInViewController : UIViewController
+
 @property (strong, nonatomic) IBOutlet UILabel *statusLabel;
 
 @end
